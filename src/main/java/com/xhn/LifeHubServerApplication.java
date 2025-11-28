4a011e0e2693bc15bc58386a403bb31ca20b1fb7
肖海南 <hain.work@outlook.com>
2025-11-28 15:46:36 +0800

    初始化

---
+++ src/main/java/com/xhn/LifeHubServerApplication.java
@@ -0,0 +1,13 @@
+package com.xhn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifeHubServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LifeHubServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xhn/LifeHubServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xhn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifeHubServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
